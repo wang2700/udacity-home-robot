7a974d785cad2f00afef0175ad3d99856e53a4c1
Jerry Wang <jerrywangpico@gmail.com>
2021-04-22 22:12:55 -0400

    add marker simulation

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -4,6 +4,6 @@ xterm -e " source devel/setup.bash && export TURTLEBOT_GAZEBO_WORLD_FILE="/home/
 sleep 15
 xterm -e " export TURTLEBOT_GAZEBO_MAP_FILE="/home/workspace/catkin_ws/src/maps/map.yaml" && roslaunch turtlebot_gazebo amcl_demo.launch " &
 sleep 15
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm -e " rosrun rviz rviz -d "/home/workspace/catkin_ws/src/rvizConfig/navigation.rviz" " &
 sleep 15
-xterm -e " roslaunch add_markers add_markers.launch " &
+xterm -e " roslaunch add_markers marker_simulation.launch " &
--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -4,7 +4,7 @@ xterm -e " source devel/setup.bash && export TURTLEBOT_GAZEBO_WORLD_FILE="/home/
 sleep 15
 xterm -e " export TURTLEBOT_GAZEBO_MAP_FILE="/home/workspace/catkin_ws/src/maps/map.yaml" && roslaunch turtlebot_gazebo amcl_demo.launch " &
 sleep 15
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm -e " rosrun rviz rviz -d "/home/workspace/catkin_ws/src/rvizConfig/navigation.rviz" " &
 sleep 15
 xterm -e " roslaunch pick_objects pick_object.launch " &
 xterm -e " roslaunch add_markers add_markers.launch " &
